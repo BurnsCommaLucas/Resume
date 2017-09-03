ecf4602a159b889b9709b3bceac40dbae2a72b87
Lucas Burns <lucas@burns.io>
2017-09-03 11:20:28 -0700

    Centered pdf text on small screen

--- build.bash
+++ build.bash
@@ -15,6 +15,13 @@ echo '##########################################################'
 
 pandoc "$DIR/resume.tex" -o "$DIR/resume.html"
 
+# misguided attempt to left aligh coursework
+# sed -i -e 's/<dd><p>Computer Networks/<dd><p id="coursework">Computer Networks/' "$DIR/resume.html"
+
+echo '</div>' >> "$DIR/resume.html"
+echo '</body>' >> "$DIR/resume.html"
+echo '</html>' >> "$DIR/resume.html"
+
 cat "$DIR/header.html" "$DIR/resume.html" > "$DIR/resume.tmp"
 mv -v "$DIR/resume.html" ~/.Trash
 mv "$DIR/resume.tmp" "$DIR/resume.html"
